efa72416a3ef4e0d93b2d3a5b2fb9273199f5ce1
Michael Ho <michael.ho01@verizonmedia.com>
2020-04-13 17:04:22 -0700

    Project init

---
+++ LeetCodeChallenge/LeetCodeChallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCodeChallenge
+//
+//  Created by Michael Ho on 4/13/20.
+//  Copyright © 2020 Michael Ho. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
