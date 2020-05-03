9abdbd4b267a132a8dec1ae7fd378ee8940fd45f
Michael Ho <twho@umich.edu>
2020-05-02 22:41:32 -0700

    Add day 30 solution

--- LeetCodeChallenge/LeetCodeChallenge/Week5/SolutionDay30.swift
+++ LeetCodeChallenge/LeetCodeChallenge/Week5/SolutionDay30.swift
@@ -11,8 +11,25 @@
 // https://thefellowprogrammer.blogspot.com/2020/04/check-if-string-is-valid-sequence-from.html
 class SolutionDay30 {
     
-    func isValidSequence(_ root: TreeNode?, _ arr: [Int]) -> Bool{
+    func isValidSequence(_ root: TreeNode?, _ arr: [Int]) -> Bool {
+        var current = [Int]()
+        return isValidSequence(root, &current, arr)
+    }
+    
+    private func isValidSequence(_ root: TreeNode?, _ current: inout [Int], _ target: [Int]) -> Bool {
+        guard let root = root else {
+            return false
+        }
         
-        return false
+        current.append(root.val)
+        if let left = root.left, let right = root.right {
+            return isValidSequence(left, &current, target) || isValidSequence(right, &current, target)
+        } else if let left = root.left {
+            return isValidSequence(left, &current, target)
+        }  else if let right = root.right {
+            return isValidSequence(right, &current, target)
+        } else {
+            return current == target
+        }
     }
 }
