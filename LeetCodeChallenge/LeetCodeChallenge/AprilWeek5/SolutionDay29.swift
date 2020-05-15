e6945e8025a0a37a50ebe78bb566802d4e660cef
Michael Ho <twho@umich.edu>
2020-05-15 10:37:26 -0700

    Add comments to day 29 solution

--- LeetCodeChallenge/LeetCodeChallenge/AprilWeek5/SolutionDay29.swift
+++ LeetCodeChallenge/LeetCodeChallenge/AprilWeek5/SolutionDay29.swift
@@ -7,6 +7,21 @@
 //
 
 // LeetCode: https://leetcode.com/problems/binary-tree-maximum-path-sum/
+/**
+ Subproblems:
+ 
+ 1. How to calculate a one-way path that has the largest value?
+ Let's define a leg as the one-way path that does not pass the
+ local root, which is the starting nodepassed to the recursion.
+ 
+ 2. How to find the local max-value path of a node?
+ It consists of up to two non-overlapping legs since it may or may
+ not pass the root. That said, legs that share the same root may not
+ be included simultaneously.
+ 
+ 3. How to find the max-value path of the entire tree?
+ It will be obtained by comparing the local max-value paths.
+ */
 class AprilSolDay29 {
     
     func maxPathSum(_ root: TreeNode?) -> Int {
@@ -29,8 +44,26 @@ class AprilSolDay29 {
         if let right = root.right {
             rightSum = getSum(right, &output)
         }
-       
-        output = max(output, root.val, root.val + leftSum, root.val + rightSum, root.val + rightSum + leftSum)
-        return max(root.val + leftSum, root.val + rightSum, root.val)
+        
+        let largestLegValue = max(root.val + leftSum, root.val + rightSum, root.val)
+        
+        /**
+         This comparison is trying to find the local maximum value staring from
+         the local root node.
+         */
+        let localMaxValue = max(root.val + rightSum + leftSum, largestLegValue)
+        
+        /**
+         The comparison here is trying to get the final output, which is the
+         max-value path of the entire tree.
+         */
+        output = max(output, localMaxValue)
+        
+        /**
+         The return value here is the leg value, aka the one-way path that has the largest
+         value. Because there might be negative values in the legs, we have to first decide
+         if we want to include a leg value. If we do, we want to take the leg with larger value.
+         */
+        return largestLegValue
     }
 }
